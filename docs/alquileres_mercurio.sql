96dbd985f6e8a2bf998db5606def75098b991df1
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2022-04-10 01:06:51 +0200

    Modelo de base de datos

---
+++ docs/alquileres_mercurio.sql
@@ -0,0 +1,79 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.28 - MySQL Community Server - GPL
+-- SO del servidor:              Linux
+-- HeidiSQL Versión:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para alquileres_mercurio
+CREATE DATABASE IF NOT EXISTS `alquileres_mercurio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alquileres_mercurio`;
+
+-- Volcando estructura para tabla alquileres_mercurio.alquileres
+CREATE TABLE IF NOT EXISTS `alquileres` (
+  `id` bigint NOT NULL,
+  `comentario` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `fecha_fin` date NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `pago` double DEFAULT NULL,
+  `id_cliente` bigint DEFAULT NULL,
+  `id_vehiculo` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKc0pat9t160g6dj3xvm1x66mcx` (`id_cliente`),
+  KEY `FKhglefsdc22bii9ak42yyxnoo7` (`id_vehiculo`),
+  CONSTRAINT `FKc0pat9t160g6dj3xvm1x66mcx` FOREIGN KEY (`id_cliente`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `FKhglefsdc22bii9ak42yyxnoo7` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla alquileres_mercurio.hibernate_sequence
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla alquileres_mercurio.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `dni` varchar(9) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `telefono` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_kfsp0s1tflm1cwlj8idhqsad0` (`email`),
+  UNIQUE KEY `UK_ggd9d47p8x7m0ajavk1ayuyqs` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla alquileres_mercurio.vehiculos
+CREATE TABLE IF NOT EXISTS `vehiculos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `consumo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `estado` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `matricula` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `modelo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `tipo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_1sakesykudfm65xwe6eboc3o8` (`tipo`),
+  UNIQUE KEY `UK_widpc0i9uotdrlq5xxlklr0d` (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
